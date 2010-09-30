9386ef60249e9e154e2b5a405717c841c515680d
Jon Fuller <fullerjc@gmail.com>
2010-09-30 07:57:22 -0400

    fixed admin scaffolding, started to add seeding to tournament management

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -4,4 +4,6 @@ class Tournament
   field :start_time, :type => Date
   embeds_many :seeding, :class_name => "Seeding"
   embeds_one :opening_round, :class_name => "Game"
+
+  accepts_nested_attributes_for :seeding
 end
