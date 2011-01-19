6eb3f78015d63caa680e622c296a63b15bf45b35
Brian J Ball <myotherpants@gmail.com>
2011-01-19 12:23:08 -0500

    added initial CSS with some ugly colors for debug

--- app/helpers/bracket_helper.rb
+++ app/helpers/bracket_helper.rb
@@ -1,2 +1,8 @@
 module BracketHelper
+  def one_side(p)
+    "one_" +(p.game_number <= 16 ? "left" : "right")
+  end
+  def zero_side(p)
+    "zero_" + (p.game_number <= 2 ? "left" : "right")
+  end
 end
